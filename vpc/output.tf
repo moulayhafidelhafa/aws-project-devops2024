7f2c61622c62e336bc48a79296c0b687aee098e9
moulayhafidelhafa <167933385+moulayhafidelhafa@users.noreply.github.com>
2024-07-13 22:05:39 -0500

    added comment

--- vpc/output.tf
+++ vpc/output.tf
@@ -1,16 +1,16 @@
-output "vpc_id" {
+output "vpc_id" {                           #"The ID of VPC"
   value = module.vpc.vpc_id
 }
 
-output public_subnets {
+output public_subnets {                      #"The IDs of the public subnetes"
   value = module.vpc.public_subnets
 }
 
-output private_subnets {
+output private_subnets {                    #The IDs of thr private subnets
   value = module.vpc.private_subnets
 }
 
-output "natgw_ids" {
+output "natgw_ids" {                        #THE ID of the natgw
     value = module_vpc natgw_ids
 }
 
